d400868f03f2d837bc8331bb31baf1f8c41dacf6
Chris Lucas <chris@chrisjlucas.com>
2013-03-27 20:03:39 -0700

    prevent loading of unused modules

--- net.cjlucas.alfred.tower/alp/__init__.py
+++ net.cjlucas.alfred.tower/alp/__init__.py
@@ -4,32 +4,32 @@ try:
 except ImportError:
     pass
 
-try:
-    from .keychain import *
-except ImportError:
-    pass
+# try:
+    # from .keychain import *
+# except ImportError:
+    # pass
 
-try:
-    from .settings import *
-except ImportError:
-    pass
+# try:
+    # from .settings import *
+# except ImportError:
+    # pass
 
-try:
-    from .mail import *
-except ImportError:
-    pass
+# try:
+    # from .mail import *
+# except ImportError:
+    # pass
 
 try:
     from .fuzzy import *
 except ImportError:
     pass
 
-try:
-    from alp.request.request import *
-except ImportError:
-    pass
+# try:
+    # from alp.request.request import *
+# except ImportError:
+    # pass
 
-try:
-    from .notification import *
-except ImportError:
-    pass
+# try:
+    # from .notification import *
+# except ImportError:
+    # pass
