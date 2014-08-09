eea6225cefa413863b47d5e58bd47cd29e78bf25
Chris Lucas <chris@chrisjlucas.com>
2014-08-09 15:57:50 -0700

    Implemented bookmarks parser

--- net.cjlucas.alfred.tower/lib/tower_bookmarks_parser.rb
+++ net.cjlucas.alfred.tower/lib/tower_bookmarks_parser.rb
@@ -2,11 +2,28 @@ require 'rexml/document'
 
 module TowerWorkflow
   class Folder
-    attr_reader :sub_folders, :repositories
+    attr_reader :name, :sub_folders, :repositories
 
-    def initialize
+    def initialize(name = nil)
+      @name = name
       @sub_folders = []
       @repositories = []
+    end
+
+    def <<(object)
+      if object.is_a?(Folder)
+        @sub_folders << object
+      elsif object.is_a?(Repository)
+        @repositories << object
+      end
+    end
+
+    def has_folder?(folder_name)
+      !@sub_folders.select { |folder| folder.name.eql?(folder_name) }.empty?
+    end
+
+    def has_repository?(repository_name)
+      !@repositories.select { |repo| repo.name.eql?(repository_name)}.empty?
     end
   end
 
@@ -20,8 +37,8 @@ module TowerWorkflow
   end
 
   class BookmarksParser
-    def self.get_folders(bookmarks_path)
-      new(bookmarks_path).get_folders
+    def self.get_bookmarks(bookmarks_path)
+      new(bookmarks_path).get_bookmarks
     end
 
     def initialize(bookmarks_path)
@@ -31,46 +48,92 @@ module TowerWorkflow
       @xml = REXML::Document.new(path)
     end
 
-    def get_folders
-      []
+    def get_bookmarks
+      @parsed ||= parse
+
+      require 'awesome_print'
+      ap @parsed
+      process_parsed_data(@parsed)
     end
 
     private
-    #
-    # def process_dict_element(dict_elem)
-    #   dict = {}
-    #   current_key = nil
-    #
-    #   dict_elem.each_element do |elem|
-    #     if elem.name.eql?('key')
-    #       current_key = elem.text
-    #       next
-    #     end
-    #
-    #     dict[current_key] = case elem.name
-    #                         when 'string'
-    #                           elem.text
-    #                         when 'dict'
-    #                           parse_dict_element(elem)
-    #                         when 'array'
-    #                           []
-    #                         end
-    #   end
-    #
-    #   dict
-    # end
-    #
-    # def process_array_element(array_elem)
-    #   array = []
-    #   array_elem.get_elements('dict').each { |elem| process_dict_element(elem) }
-    # end
-    #
-    # def parse
-    #   root_dict = @xml.elements.to_a.first.elements('dict').first
-    #
-    #   root_dict.get_elements('array').each do |array_elem|
-    #   end
-    # end
+
+    def process_parsed_data(data)
+      Folder.new.tap do |top_level_folder|
+        data.each { |hash| process_parsed_hash(hash, top_level_folder) }
+      end
+    end
+
+    def process_parsed_hash(hash, current_folder)
+      if valid_repository?(hash)
+        current_folder << repository_from_hash(hash)
+      elsif folder?(hash)
+        folder = folder_from_hash(hash)
+        current_folder << folder
+        hash['children'].each { |child| process_parsed_hash(child, folder) }
+      end
+    end
+
+    def valid_repository?(hash)
+      hash.has_key?('filereferenceurl') && hash.has_key?('fileurl')
+    end
+
+    def folder?(hash)
+      hash.size == 2 && hash.has_key?('children') && hash.has_key?('name')
+    end
+
+    def repository_from_hash(hash)
+      Repository.new(hash['name'], hash['fileurl'][7, hash['fileurl'].length])
+    end
+
+    def folder_from_hash(hash)
+      Folder.new(hash['name'])
+    end
+
+    ## XML Parsing
+
+    def process_dict_element(dict_elem)
+      dict = {}
+      current_key = nil
+
+      dict_elem.each_element do |elem|
+        if elem.name.eql?('key')
+          current_key = elem.text.downcase
+          next
+        end
+
+        dict[current_key] = case elem.name
+                            when 'string'
+                              elem.text
+                            when 'dict'
+                              parse_dict_element(elem)
+                            when 'array'
+                              process_array_element(elem)
+                            end
+      end
+
+      dict
+    end
+
+    def process_array_element(array_elem)
+      array = []
+      array_elem.get_elements('dict').each do |elem|
+        array << process_dict_element(elem)
+      end
+
+      array
+    end
+
+    def parse
+      root_dict = @xml.elements.to_a.first.get_elements('dict').first
+
+      bookmarks = []
+      root_dict.get_elements('array').each do |array_elem|
+        bookmarks += process_array_element(array_elem)
+      end
+
+      bookmarks
+    end
   end
 end
 
--- net.cjlucas.alfred.tower/spec/tower_bookmarks_parser_spec.rb
+++ net.cjlucas.alfred.tower/spec/tower_bookmarks_parser_spec.rb
@@ -1,18 +1,59 @@
 require_relative 'spec_helper'
 
+class TowerWorkflow::Folder
+  def folders_named(folder_name)
+    sub_folders.select { |folder| folder.name.eql?(folder_name) }
+  end
+
+  def repositories_named(repository_name)
+    repositories.select { |repo| repo.name.eql?(repository_name) }
+  end
+end
+
 describe TowerWorkflow::BookmarksParser do
   context 'with a single empty folder' do
-    before(:each) do
+    before(:all) do
       bookmarks_file = data_file('bookmarks_empty_folder.xml')
-      @folders = described_class.get_folders(bookmarks_file)
+      @bookmarks = described_class.get_bookmarks(bookmarks_file)
+      @empty_folder = @bookmarks.folders_named('Empty Folder').first
     end
 
-    it '#get_folders should return one empty Folder' do
-      expect(@folders.size).to eq(1)
+    it 'should contain one folder' do
+      expect(@empty_folder).not_to be_nil
+    end
 
-      folder = @folders.first
-      expect(folder.repositories).to be_empty
-      expect(folder.sub_folders).to be_empty
+    it 'should contain zero repositories' do
+      expect(@bookmarks.repositories).to be_empty
+      expect(@empty_folder.repositories).to be_empty
+    end
+
+    it 'should contain zero sub folders' do
+      expect(@empty_folder.sub_folders).to be_empty
+    end
+  end
+
+  context 'with multiple folders (one level deep)' do
+    before(:all) do
+      bookmarks_file = data_file('bookmarks_multiple_folders.xml')
+      @bookmarks = described_class.get_bookmarks(bookmarks_file)
+    end
+
+    it 'should contain all folders' do
+      expect(@bookmarks.folders_named('Folder 1').size).to eq(1)
+      expect(@bookmarks.folders_named('Folder 2').size).to eq(1)
+    end
+
+    it 'should contain all repositories' do
+      expect(@bookmarks.repositories_named('Default Folder, Repo 1').size).to eq(1)
+
+      folder = @bookmarks.folders_named('Folder 1').first
+      expect(folder.has_repository?('Folder 1, Repo 1')).to be_truthy
+      expect(folder.has_repository?('Folder 1, Repo 2')).to be_truthy
+      expect(folder.has_repository?('Folder 1, Repo 3')).to be_truthy
+
+      folder = @bookmarks.folders_named('Folder 2').first
+      expect(folder.has_repository?('Folder 2, Repo 1')).to be_truthy
+      expect(folder.has_repository?('Folder 2, Repo 2')).to be_truthy
     end
   end
 end
